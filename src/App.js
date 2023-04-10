cc2ae1c70743ce9986488f4bd2e7b5eb5d4c5941
Doryan <d.prete@debussac.net>
2023-04-10 17:04:29 +0200

    feat: add popup message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GridBoard from "./components/GridBoard";
 import NextBlock from "./components/NextBlock";
 import ScoreBoard from "./components/ScoreBoard";
 import Controls from "./components/Controls";
+import MessagePopup from "./components/MessagePopup";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <NextBlock />
           <ScoreBoard />
           <Controls />
+          <MessagePopup />
       </div>
   );
 }
