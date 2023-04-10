709c492830a293c1381dfdc98a1fe4dec31b6e54
Doryan <d.prete@debussac.net>
2023-04-10 17:00:25 +0200

    feat: add controls buttons

---
+++ src/components/Controls.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+export default function Controls(props) {
+    return (
+        <div className="controls">
+            <button className="control-button" onClick={(e) => {
+
+            }}>Gauche</button>
+
+            <button className="control-button" onClick={(e) => {
+
+            }}>Droite</button>
+
+            <button className="control-button" onClick={(e) => {
+
+            }}>Rotation</button>
+
+            <button className="control-button" onClick={(e) => {
+
+            }}>Bas</button>
+
+        </div>
+    )
+}
+
--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -7,9 +7,9 @@ export default function ScoreBoard(props) {
             <div>Score:{ props.score }</div>
             <div>Level: 1</div>
             <button className="score-board-button" onClick={(e) => {
-            }}>Play</button>
+            }}>Jouer</button>
             <button className="score-board-button" onClick={(e) => {
-            }}>Restart</button>
+            }}>Redémarrer</button>
         </div>
     )
 }
