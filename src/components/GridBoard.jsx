a5b6a13d86fedd97641d6bb4970025b96201264a
Doryan <d.prete@debussac.net>
2023-04-11 19:23:29 +0200

    feat: add timer/pause

--- src/components/GridBoard.jsx
+++ src/components/GridBoard.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown } from '../actions'
 import GridSquare from "./GridSquare";
-import { useSelector } from 'react-redux'
 import { shapes } from '../utils/index'
 
 
 export default function GridBoard(props) {
     const game = useSelector((state) => state.game)
+    const requestRef = useRef()
+    const lastUpdateTimeRef = useRef(0)
+    const progressTimeRef = useRef(0)
+    const dispatch = useDispatch()
     const { grid, shape, rotation, x, y, isRunning, speed } = game
 
     const block = shapes[shape][rotation]
@@ -30,6 +35,28 @@ export default function GridBoard(props) {
             return <GridSquare key={k} color={color} />
         })
     })
+
+    const update = (time) => {
+        requestRef.current = requestAnimationFrame(update)
+        if (!isRunning) {
+            return
+        }
+        if (!lastUpdateTimeRef.current) {
+            lastUpdateTimeRef.current = time
+        }
+        const deltaTime = time - lastUpdateTimeRef.current
+        progressTimeRef.current += deltaTime
+        if (progressTimeRef.current > speed) {
+            dispatch(moveDown())
+            progressTimeRef.current = 0
+        }
+        lastUpdateTimeRef.current = time
+    }
+
+    useEffect(() => {
+        requestRef.current = requestAnimationFrame(update)
+        return () => cancelAnimationFrame(requestRef.current)
+    }, [isRunning])
 
     return (
         <div className='grid-board'>
