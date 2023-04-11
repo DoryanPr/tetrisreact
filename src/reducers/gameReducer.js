ec9e5e9f304fcef9a14c5bb63497d16cb350ebda
Doryan <d.prete@debussac.net>
2023-04-11 19:34:08 +0200

    feat: restart

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -77,7 +77,7 @@ const gameReducer = (state = defaultState(), action) => {
 
         case RESTART:
 
-            return state
+            return defaultState()
 
         default:
             return state
