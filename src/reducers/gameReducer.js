cc1fbfae387d9f0a71bc364cda11028463dcf855
Doryan <d.prete@debussac.net>
2023-04-11 17:58:22 +0200

    feat: add next shape

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,6 +1,7 @@
 import {GAME_OVER, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, PAUSE, RESTART, RESUME, ROTATE} from "../actions";
+import {defaultState} from "../utils";
 
-const gameReducer = (state = {}, action) => {
+const gameReducer = (state = defaultState(), action) => {
 
     // compare le type d'action
     switch(action.type) {
