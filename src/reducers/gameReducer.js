7939ec4b68bcb64b6bf0baf2fe8399bc8279bb0b
Doryan <d.prete@debussac.net>
2023-04-11 18:59:04 +0200

    feat: reducer rotate

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,12 +1,16 @@
 import {GAME_OVER, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, PAUSE, RESTART, RESUME, ROTATE} from "../actions";
-import {defaultState} from "../utils";
+import {canMoveTo, defaultState, nextRotation} from "../utils/index";
 
 const gameReducer = (state = defaultState(), action) => {
+    const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state
 
     // compare le type d'action
     switch(action.type) {
         case ROTATE:
-
+            const newRotation = nextRotation(shape, rotation)
+            if (canMoveTo(shape, grid, x, y, newRotation)) {
+                return { ...state, rotation: newRotation }
+            }
             return state
 
         case MOVE_RIGHT:
