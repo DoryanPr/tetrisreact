06649962e02fb8756ae73ef2f9bb37b16cb28d40
Doryan <d.prete@debussac.net>
2023-04-11 18:10:10 +0200

    feat: props to button controls

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
 
 export default function Controls(props) {
+    const dispatch = useDispatch()
+    const isRunning = useSelector((state) => state.isRunning)
+
     return (
         <div className="controls">
             <button className="control-button" onClick={(e) => {
-
-            }}>Gauche</button>
-
-            <button className="control-button" onClick={(e) => {
-
-            }}>Droite</button>
+                dispatch(moveLeft())
+            }}>Left</button>
 
             <button className="control-button" onClick={(e) => {
-
-            }}>Rotation</button>
+                dispatch(moveRight())
+            }}>Right</button>
 
             <button className="control-button" onClick={(e) => {
+                dispatch(rotate())
+            }}>Rotate</button>
 
-            }}>Bas</button>
+            <button className="control-button" onClick={(e) => {
+                dispatch(moveDown())
+            }}>Down</button>
 
         </div>
     )
